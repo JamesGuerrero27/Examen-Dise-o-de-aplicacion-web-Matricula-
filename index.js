19523e1cbf92c28f7ea5b5305f57ed2ab6d8e2c4
JamesGuerrero <jamessteve_27hotmail.com>
2018-11-09 01:42:13 -0600

    second commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser')
+var app = express();
+var sql = require('mssql');
+var env = require('dotenv');
+var multer = require('multer');
+var path = require('path');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now()+'.'+getExtension(file.originalname))
+    }
+})
+
+function getExtension(filename) {
+    var ext = path.extname(filename||'').split('.');
+    return ext[ext.length - 1];
+}
+
+var upload = multer({ storage: storage })
+
+
+const result = env.config();
+app.use(cors());
+app.use(bodyParser());
+
+const sqlConfig = {
+    user: process.env.DB_USER || 'sa',
+    password: process.env.DB_PASS,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    port: parseInt(process.env.DB_PORT),
+    debug: true,
+    options: {
+        encrypt: false
+    }
+};
